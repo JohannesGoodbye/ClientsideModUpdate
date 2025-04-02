1f1629fd8be1a7087369d471743e7c96ce319ba9
JohannesGoodbye <jolejo2009@yahoo.com>
2025-04-02 19:05:45 +0200

    trying to get path of executable file

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import os
+import sys
 
 # Dynamically set LOCAL_MODS_PATH based on script location
-script_dir = os.path.dirname(os.path.realpath(__file__))  # Get the directory where the script is located
-LOCAL_MODS_PATH = os.path.join(script_dir, "mods")  # Construct path to "mods" folder
+if getattr(sys, 'frozen', False):
+    # If running from a PyInstaller executable, use the location of the .exe
+    base_path = os.path.dirname(sys.executable)
+else:
+    # If running as a script, use the location of the script
+    base_path = os.path.dirname(os.path.realpath(__file__))
+
+LOCAL_MODS_PATH = os.path.join(base_path, "mods")  # Construct path to "mods" folder
 
 def locate_file_path():
     print(LOCAL_MODS_PATH)
